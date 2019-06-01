29bdeb8b2c96fdd781c9aa55da42e37655d04fac
ravilzalya <ravil.zalya@ad-juster.com>
2019-06-01 16:59:29 -0700

    commit

---
+++ src/test/java/ContainerStartedOnceTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+class ContainerStartedOnceTest {
+
+    @Container
+    private static final BrowserWebDriverContainer BROWSER_CONTAINER = new BrowserWebDriverContainer()
+            .withCapabilities(new ChromeOptions());
+
+    private static WebDriver browser;
+
+    @BeforeAll
+    static void configureBrowser() {
+        browser = BROWSER_CONTAINER.getWebDriver();
+    }
+
+    @Test
+    @DisplayName("The testproject.io web site should have the correct title")
+    void testProjectWebSiteShouldHaveCorrectTitle() {
+        browser.get("https://www.testproject.io");
+        assertThat(browser.getTitle())
+                .isEqualTo("TestProject - Community Powered Test Automation");
+    }
+
+    @Test
+    @DisplayName("The testproject.io blog should have the correct title")
+    void testProjectBlogShouldHaveCorrectTitle() {
+        browser.get("https://blog.testproject.io/");
+        assertThat(browser.getTitle())
+                .isEqualTo("TestProject - Test Automation Blog");
+    }
+
+}
