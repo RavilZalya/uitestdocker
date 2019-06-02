22013ed0bb543e6e9cd0603c4c554a730c4a4bd4
ravilzalya <ravil.zalya@ad-juster.com>
2019-06-02 00:30:05 -0700

    abcd

--- src/test/java/ContainerStartedOnceTest.java
+++ src/test/java/ContainerStartedOnceTest.java
@@ -17,7 +17,7 @@ class ContainerStartedOnceTest {
 
     @Container
     private static final BrowserWebDriverContainer BROWSER_CONTAINER = new BrowserWebDriverContainer()
-            .withCapabilities((new ChromeOptions().setHeadless(true)))
+            .withCapabilities((new ChromeOptions()))
             .withRecordingMode(VncRecordingMode.RECORD_ALL, new File("./target/")
             //.withRecordingFileFactory(new CustomRecordingFileFactory()
             );
